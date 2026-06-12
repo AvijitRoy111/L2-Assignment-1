898de20a51720737b4f4db4fc04836e4ecf329b9
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-12 11:13:44 +0600

    added the final result

--- src/Solutions.ts
+++ src/Solutions.ts
@@ -24,7 +24,7 @@
  const reverseStringResult = reverseString("typescript");
 
  console.log(reverseStringResult);
-// // Sample Output:
+ // Sample Output:
 // // "tpircsepyt";
 
 // // =========================================================
